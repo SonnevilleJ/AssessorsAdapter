98ec8ffcc216c71cf02c0ffbde5350f54b4c6566
John Sonneville <SonnevilleJohn@JohnDeere.com>
2013-03-03 17:55:37 -0600

    Initial check-in.

---
+++ AssessorsAdapter/Assessor.cs
@@ -0,0 +1,19 @@
+﻿namespace AssessorsAdapter
+{
+    public class House
+    {
+        private const string Url = "http://www.assess.co.polk.ia.us/cgi-bin/invenquery/homequery.cgi?method=GET&address={0}&photo={2}&map={3}&jurisdiction={1}";
+
+        public House(string address, string city = "COUNTY-WIDE", bool photo = true, bool map = false)
+        {
+            ProcessHtmlPage(address, city, photo, map);
+        }
+
+        private void ProcessHtmlPage(string address, string city, bool photo, bool map)
+        {
+            Address = address;
+        }
+
+        public string Address { get; private set; }
+    }
+}
---
+++ AssessorsAdapterTest/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using AssessorsAdapter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AssessorsAdapterTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string Address = "6324 Wilcot Ct";
+        private const string City = "Johnston";
+
+        private const string AndysAddress = "9260 NW 36th St";
+
+        [TestMethod]
+        public void HouseConstructor()
+        {
+            var house = new House(Address, City);
+            Assert.IsNotNull(house);
+        }
+
+        [TestMethod]
+        public void AddressMatches()
+        {
+            var house = new House(Address);
+            Assert.AreEqual(Address, house.Address);
+        }
+
+        [TestMethod]
+        public void AndysAddressMatches()
+        {
+            var house = new House(AndysAddress);
+            Assert.AreEqual(AndysAddress, house.Address);
+        }
+    }
+}
