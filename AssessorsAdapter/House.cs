23cd44042c02277e009578eae5ccd193966b49ed
John Sonneville <SonnevilleJohn@JohnDeere.com>
2013-03-03 20:07:23 -0600

    Implemented Address, City, and Zip.

---
+++ AssessorsAdapter/House.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace AssessorsAdapter
+{
+    public class House
+    {
+        private const string QueryUrl = @"http://www.assess.co.polk.ia.us/cgi-bin/invenquery/homequery.cgi?method=GET&address={0}&photo={2}&map={3}&jurisdiction={1}";
+
+        public House(string address, string city = "COUNTY-WIDE", bool photo = true, bool map = false)
+        {
+            var url = string.Format(QueryUrl, Uri.EscapeUriString(address), city.ToUpper(), photo ? "checked" : "", map ? "checked" : "");
+            var client = new HtmlWeb();
+            var doc = client.Load(url);
+
+            if (NoResultsFound(doc))
+            {
+                Process.Start("chrome", url);
+            }
+            ParseHtml(doc);
+            
+        }
+
+        public string Address { get; private set; }
+
+        public string City { get; private set; }
+
+        public string Zip { get; private set; }
+
+        private static bool NoResultsFound(HtmlDocument doc)
+        {
+            return doc.DocumentNode.InnerText.Contains("0 Records");
+        }
+
+        private void ParseHtml(HtmlDocument document)
+        {
+            var addressNode = document.DocumentNode.Descendants("a").First(node => node.InnerText == "Street Address");
+            var addressTrNode = addressNode.ParentNode.ParentNode;
+            var address = addressTrNode.NextSibling.NextSibling.InnerText.Split(new[]{'\n'}, StringSplitOptions.RemoveEmptyEntries);
+            ParseAddress(address);
+        }
+
+        private void ParseAddress(string[] address)
+        {
+            Address = RemoveSpaces(address[0]);
+            ParseCity(address[1]);
+        }
+
+        private void ParseCity(string city)
+        {
+            var strings = city.Split(new[] {" IA "}, StringSplitOptions.None);
+            City = strings[0];
+            Zip = strings[1];
+        }
+
+        private string RemoveSpaces(string addressString)
+        {
+            var builder = new StringBuilder();
+            var lastWasSpace = false;
+            foreach (var letter in addressString)
+            {
+                if (letter == ' ')
+                {
+                    if(lastWasSpace) continue;
+                    lastWasSpace = true;
+                }
+                else
+                {
+                    lastWasSpace = false;
+                }
+                builder.Append(letter);
+            }
+            return builder.ToString().Trim();
+        }
+    }
+}
---
+++ AssessorsAdapterTest/HouseTest.cs
@@ -0,0 +1,84 @@
+﻿using AssessorsAdapter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AssessorsAdapterTest
+{
+    [TestClass]
+    public class HouseTest
+    {
+        private const string Address = "6324 Wilcot Ct";
+        private const string City = "Johnston";
+        private const int ExpectedAssessment = 291600;
+
+        private const string AndysAddress = "9260 NW 36th St";
+
+        [TestMethod]
+        public void HouseConstructor()
+        {
+            var house = new House(Address, City);
+            Assert.IsNotNull(house);
+        }
+
+        [TestMethod]
+        public void AddressMatches()
+        {
+            var house = new House(Address);
+            VerifyAddressMatches(Address, house);
+        }
+
+        [TestMethod]
+        public void CityMatches()
+        {
+            var house = new House(Address);
+            VerifyCityMatches(City, house);
+        }
+
+        [TestMethod]
+        public void ZipMatches()
+        {
+            var house = new House(Address);
+            VerifyZipMatches(50131, house);
+        }
+
+        private static void VerifyAddressMatches(string address, House house)
+        {
+            Assert.AreEqual(address, house.Address, true);
+        }
+
+        private void VerifyCityMatches(string city, House house)
+        {
+            Assert.AreEqual(city, house.City, true);
+        }
+
+        private void VerifyZipMatches(int zip, House house)
+        {
+            VerifyZipMatches(zip.ToString(), house);
+        }
+
+        private static void VerifyZipMatches(string zip, House house)
+        {
+            Assert.AreEqual(zip, house.Zip);
+        }
+
+        [TestMethod]
+        public void AndysAddressMatches()
+        {
+            var house = new House(AndysAddress);
+            VerifyAddressMatches(AndysAddress, house);
+        }
+
+        [TestMethod]
+        public void AndysCityMatches()
+        {
+            var house = new House(AndysAddress);
+            VerifyCityMatches("Polk City", house);
+        }
+
+        [TestMethod]
+        public void AndysZipMatches()
+        {
+            var house = new House(AndysAddress);
+            VerifyZipMatches(50226, house);
+        }
+    }
+}
