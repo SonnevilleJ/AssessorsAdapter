4d0a2d6884259397de0e12ace2b370c2bf147cf0
John Sonneville <SonnevilleJohn@JohnDeere.com>
2013-03-13 21:14:00 -0500

    Initial check-in for CachingRepository.

---
+++ AssessorsAdapter/Persistence/CachingRepository.cs
@@ -0,0 +1,35 @@
+﻿namespace AssessorsAdapter.Persistence
+{
+    public class CachingRepository<TKey, TValue> : IRepository<TKey, TValue>
+    {
+        private readonly IRepository<TKey, TValue> _master;
+        private readonly IRepository<TKey, TValue> _cache;
+
+        public CachingRepository(IRepository<TKey, TValue> master, IRepository<TKey, TValue> cache)
+        {
+            _master = master;
+            _cache = cache;
+        }
+
+        public void Save(TKey key, TValue value)
+        {
+            _cache.Save(key, value);
+        }
+
+        public void Delete(TKey key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool ContainsValue(TValue value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            var cacheContains = _cache.ContainsKey(key);
+            return cacheContains || _master.ContainsKey(key);
+        }
+    }
+}
---
+++ AssessorsAdapterTest/Persistence/CachingRepositoryTests.cs
@@ -0,0 +1,58 @@
+﻿using AssessorsAdapter.Persistence;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace AssessorsAdapterTest.Persistence
+{
+    [TestClass]
+    public class CachingRepositoryTests
+    {
+        private Mock<IRepository<int, string>> _cacheMock;
+        private Mock<IRepository<int, string>> _masterMock;
+        private CachingRepository<int, string> _cachingRepository;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _cacheMock = new Mock<IRepository<int, string>>();
+            _masterMock = new Mock<IRepository<int, string>>();
+            _cachingRepository = new CachingRepository<int, string>(_masterMock.Object, _cacheMock.Object);
+        }
+
+        [TestMethod]
+        public void SavesToCache()
+        {
+            const int key = 1;
+            const string value = "Hello";
+
+            _cachingRepository.Save(key, value);
+
+            _cacheMock.Verify(repo => repo.Save(key, value), Times.Once());
+            _masterMock.Verify(repo => repo.Save(It.IsAny<int>(), It.IsAny<string>()), Times.Never());
+        }
+
+        [TestMethod]
+        public void ContainsKeyWhenCacheContains()
+        {
+            const int key = 1;
+            _cacheMock.Setup(repo => repo.ContainsKey(key)).Returns(true);
+            
+            _cachingRepository.ContainsKey(key);
+
+            _cacheMock.Verify(repo => repo.ContainsKey(key), Times.Once());
+            _masterMock.Verify(repo => repo.ContainsKey(It.IsAny<int>()), Times.Never());
+        }
+
+        [TestMethod]
+        public void ContainsKeyWhenCacheDoesNotContain()
+        {
+            const int key = 1;
+            _masterMock.Setup(repo => repo.ContainsKey(key)).Returns(true);
+            
+            _cachingRepository.ContainsKey(key);
+
+            _cacheMock.Verify(repo => repo.ContainsKey(key), Times.Once());
+            _masterMock.Verify(repo => repo.ContainsKey(key), Times.Once());
+        }
+    }
+}
